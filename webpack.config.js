6b6cac07d1c261dd82dea353cffa882c670d35ef
Raymond Selzer <raymond@raymondselzer.net>
2024-08-29 00:04:09 -0400

    init webpack and ts

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+
+module.exports = {
+
+  entry: './src/index.ts',
+
+  output: {
+
+    filename: 'detect-monkey-patches.js',
+
+    path: path.resolve(__dirname, 'dist'),
+
+  },
+
+  module: {
+
+    rules: [
+
+      {
+
+        test: /\.tsx?$/,
+
+        use: 'ts-loader',
+
+        exclude: /node_modules/,
+
+      },
+
+    ],
+
+  },
+
+  resolve: {
+
+    extensions: ['.tsx', '.ts', '.js'],
+
+  },
+
+  devtool: 'source-map',
+
+  devServer: {
+
+    static : {
+        directory: path.join(__dirname,'dist')
+    },
+
+    compress: true,
+
+    port: 9000,
+
+  },
+
+};
